93e6f5688b3cd77546fde4fc3e4393bc6a2fbd8d
balutarock <balutarock71117@gmail.com>
2025-11-15 16:37:31 +0530

    fix: removed unused function

--- client/src/features/dashboard/pages/dashboard.tsx
+++ client/src/features/dashboard/pages/dashboard.tsx
@@ -6,7 +6,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 
 export const DashboardPage = () => {
-  const { user, logout } = useAuthStore();
+  const { user } = useAuthStore();
   const navigate = useNavigate();
   const subscriptionStatus = user?.subscription?.status || 'none';
 
